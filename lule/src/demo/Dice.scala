f43c6fa07fd577ec44f5c31a247c515cf15a1044
matte <matte@openresult.se>
2021-02-07 20:44:15 +0100

    papper

---
+++ lule/src/demo/Dice.scala
@@ -0,0 +1,23 @@
+package demo
+import scala.io.StdIn.readLine
+import scala.util.Random
+
+object Dice {
+
+  def game() = {
+    val rnd = new Random
+    val guess = readLine("gissa vad det blir av två tärningar: ").toInt
+    val dice1 = rnd.between(1, 7)
+    val dice2 = rnd.between(1, 7)
+    val summa = dice1 + dice2
+    println("")
+
+    if (summa == guess)
+      println("Rätt svar")
+    else {
+      val diff = Math.abs(guess - summa)
+      println(s"Diff fel $diff")
+    }
+
+  }
+}
